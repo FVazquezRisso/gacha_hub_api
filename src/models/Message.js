a5d8d1513f5b1d93d60fab20bf1ec46f83fbfbbc
FVazquezRisso <facundovazquez321@gmail.com>
2024-01-18 21:48:55 -0300

    Se creó el modelo Message

---
+++ src/models/Message.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "Message",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+    },
+    {
+      paranoid: true,
+    }
+  );
+};
